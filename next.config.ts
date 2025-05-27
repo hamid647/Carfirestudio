9291f0afad94c16326e53cab12045d2cbdb4fd3a
Hamid Raza <hamidraza927660@gmail.com>
2025-05-27 11:44:07 +0000

    deploy thisproperly to firebase hosting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Add this line for static site generation
   typescript: {
     ignoreBuildErrors: true,
   },
